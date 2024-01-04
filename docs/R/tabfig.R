94640ba18f54aef23935f3cf0062949a477e28cc
Michael Friendly <friendly@yorku.ca>
2024-01-04 11:57:11 -0500

    add resources; re-build

--- docs/R/tabfig.R
+++ docs/R/tabfig.R
@@ -1,4 +1,6 @@
 library(htmltools)
+library(glue)
+
 # table tags
 tab <- function (...)
   tags$table(...)
@@ -22,4 +24,36 @@ tabfig <- function(name, img, href, ...) {
 }
 tabtxt <- function(text, ...) {
   td(text, ...)
+}
+
+TabFigText <- function(author, name,
+                       img, website, title, desc, extra,
+                       width = "160px", ...) {
+  Fig <- tabfig(name = author, img = img, href = website, width = width, ...)
+
+  link <- a(href=website, desc)
+  text <-  c(name, title, link)
+  if(!missing(extra)) text <- c(text, "br()", extra) 
+  Txt <- tabtxt(text, ...)
+
+  cat(paste(Fig, collapse = "\n"),
+      paste(Txt, collapse = "\n"))
+}
+
+if(FALSE) {
+  width <- "160px"
+  tabfig("Wainer", "images/books/Wainer-graphic-discovery.png",
+         "https://press.princeton.edu/titles/7820.html", width=width)
+  tabtxt("Howard Wainer,",  a("Graphic discovery: a trout in the milk and other visual adventures",
+                              href="https://press.princeton.edu/titles/7820.html"),
+         ". A collection of essays on the history of graphics and other topics.")
+  
+  TabFigText(
+    author = "Wainer",
+    name = "Howard Wainer",
+    img = "images/books/Wainer-graphic-discovery.png",
+    title = "Graphic discovery: a trout in the milk and other visual adventures",
+    website = "https://press.princeton.edu/titles/7820.html",
+    desc = "A collection of essays on the history of graphics and other topics."
+  )
 }
