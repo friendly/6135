18ba99dcc8e84914cb32d820a75ed3784da2ede0
Michael Friendly <friendly@yorku.ca>
2025-02-05 10:00:17 -0500

    re-build

--- docs/R/arbuthnot-gg.R
+++ docs/R/arbuthnot-gg.R
@@ -1,15 +1,32 @@
 #' ---
 #' title: "Plot Arbuthnot's data with ggplot2"
 #' author: "Michael Friendly"
-#' date: "13 Sep 2018"
+#' date: "`r format(Sys.Date())`"
+#' output:
+#'   html_document:
+#'     theme: readable
+#'     code_download: true
+#'   word_document: default    
 #' ---
 
-#' ## Load the data & ggplot2
-data(Arbuthnot, package="HistData")
-library(ggplot2) # Plots Using the Grammar of Graphics  
+#+ echo=FALSE
+knitr::opts_chunk$set(warning=FALSE, 
+                      message=FALSE, 
+                      R.options=list(digits=4))
 
-#' ## basic plot: points & lines
-#' 
+#' John Arbuthnot (1710) gathered data on the ratios of male to female christenings in London from 1629-1710 to 
+#' carry out the first known significance test, comparing observed data to a null hypothesis. 
+#' The data for these 81 years showed that in every year there were more male than female christenings.
+#' Let's make a plot for him.
+#'
+#' ## Load the data & ggplot2
+library(ggplot2) # Plots Using the Grammar of Graphics  
+data(Arbuthnot, package="HistData")
+head(Arbuthnot)
+
+#' ## Basic plot: points & lines
+#' I assign the initial plot to a variable, `arbuthplot`, so I can add additional graphical elements to it.
+#' I also expand the Y axis limits a bit to allow for annotations.
 arbuthplot <-
 ggplot(Arbuthnot, aes(x=Year, y=Ratio)) +
   ylim(1, 1.20) + 
@@ -18,12 +35,16 @@ ggplot(Arbuthnot, aes(x=Year, y=Ratio)) +
   geom_line(color="gray") 
 arbuthplot
 
-#' ## add smooths
+#' ## Add smooths
+#' He might have been interested in whether there was any trend over time. We can add a linear regression line
+#' and loess smooth using `geom_smooth`.
 arbuthplot +
   geom_smooth(method="loess", color="blue", fill="blue", alpha=0.2) +
   geom_smooth(method="lm", color="darkgreen", se=FALSE)  
 
-#' ## add reference line, annotations and change the theme
+#' ## Add reference line, annotations and change the theme
+#' To highlight that a sex ratio = 1.0 is the null hypothesis, add a thick red line at that value.
+#' Instead of a plot title outside the plot frame, you can add text annotations inside.
 arbuthplot +
   geom_smooth(method="loess", color="blue", fill="blue", alpha=0.2) +
   geom_smooth(method="lm", color="darkgreen", se=FALSE) +
